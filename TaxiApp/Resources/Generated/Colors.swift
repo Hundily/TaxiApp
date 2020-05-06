02c7724d894dbc893433147878b37702b8642c1f
Hundily Cerqueira <hundily.cerqueira@gmail.com>
2020-05-06 17:12:37 -0300

    updated assests and colors

--- TaxiApp/Resources/Generated/Colors.swift
+++ TaxiApp/Resources/Generated/Colors.swift
@@ -25,6 +25,9 @@ internal struct ColorName {
   /// <span style="display:block;width:3em;height:2em;border:1px solid black;background:#1152fd"></span>
   /// Alpha: 100% <br/> (0x1152fdff)
   internal static let blue = ColorName(rgbaValue: 0x1152fdff)
+  /// <span style="display:block;width:3em;height:2em;border:1px solid black;background:#e5e5e5"></span>
+  /// Alpha: 100% <br/> (0xe5e5e5ff)
+  internal static let grayBorder = ColorName(rgbaValue: 0xe5e5e5ff)
   /// <span style="display:block;width:3em;height:2em;border:1px solid black;background:#111112"></span>
   /// Alpha: 100% <br/> (0x111112ff)
   internal static let grayStrong = ColorName(rgbaValue: 0x111112ff)
