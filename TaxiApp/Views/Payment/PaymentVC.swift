6f73680aab7ed5b9cb53eedf7674b5cb98d3911b
Hundily Cerqueira <hundily.cerqueira@gmail.com>
2020-05-19 17:58:21 -0300

    new screen add_card_form

--- TaxiApp/Views/Payment/PaymentVC.swift
+++ TaxiApp/Views/Payment/PaymentVC.swift
@@ -1,8 +1,8 @@
 //
-//  PaymentVC.swift
+//  PaymentVC2.swift
 //  TaxiApp
 //
-//  Created by Hundily Cerqueira on 23/04/20.
+//  Created by Hundily Cerqueira on 18/05/20.
 //  Copyright © 2020 Hundily Cerqueira. All rights reserved.
 //
 
@@ -10,15 +10,58 @@ import UIKit
 
 class PaymentVC: UIViewController {
     
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = .none
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    private var kPaymentCell = "PaymentCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .white
+        setupViews()
+        tableView.registerNib(PaymentCell.self)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.navigationBar.isTranslucent = false
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        view.backgroundColor = .white
         navigationController?.navigationBar.isHidden = false
-        navigationItem.title = "Payment"
+        navigationItem.title = "Add card"
+    }
+}
+
+extension PaymentVC: CodeViewProtocol {
+    func buildViewHierarchy() {
+        view.addSubview(tableView)
     }
     
+    func setupConstraints() {
+        tableView.fullSuperview()
+    }
+}
+
+extension PaymentVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 450
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: kPaymentCell) as! PaymentCell
+        
+        return cell
+    }
 }
